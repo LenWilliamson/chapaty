30bed2e3e78591a9925153d3232091837ec0c1b6
LenWilliamson <lencewa13@gmail.com>
2023-08-28 10:43:30 +0200

    update dependencies

--- tests/poc_resistance_it.rs
+++ tests/poc_resistance_it.rs
@@ -12,14 +12,14 @@ async fn it_test() {
     let strategy = common::setup_strategy();
     let data_provider = common::setup_data_provider();
     // let years = vec![2022, 2021, 2020, 2019, 2018, 2017];
-    let years = vec![2022];
+    let years = vec![2019];
     let markets = vec![
         MarketKind::AudUsdFuture,
-        MarketKind::EurUsdFuture,
-        MarketKind::GbpUsdFuture,
-        MarketKind::CadUsdFuture,
+        // MarketKind::EurUsdFuture,
+        // MarketKind::GbpUsdFuture,
+        // MarketKind::CadUsdFuture,
         // MarketKind::YenUsdFuture,
-        MarketKind::NzdUsdFuture,
+        // MarketKind::NzdUsdFuture,
         // MarketKind::BtcUsdFuture,
     ];
     let market_simulation_data = MarketSimulationDataKind::Ohlc1m;
